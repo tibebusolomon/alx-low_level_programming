ef970340729dfaa6918e71959597ce3fdae9e5cb
tibebu <tibebu.solomonhi@gmail.com>
2023-03-26 12:53:08 -0400

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that prints a line
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return (1);
+}
+
