34f2a8742eef27bcf56c4646d498f317741dbdc5
tibebu <tibebu.solomonhi@gmail.com>
2023-03-26 12:18:00 -0400

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * main - Print the size of various types on the computer it is compiled and run on.
  * Return: Always 0 (Success)
  */
 
