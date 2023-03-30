0b6599d6d113295088503ea42a892ccd32a7d38e
tibebu <tibebu.solomonhi@gmail.com>
2023-03-30 06:01:58 -0400

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,18 @@
  */
 char *_strncpy(char *dest, chsr *src, int n)
 {
-	int i = 0;
+	int j = 0;
 
-	i = 0;
-	while (i < n && src[i] != '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 		j++;
 	}
-	while (i < n)
+	while (j < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[j] = '\0';
+		j++;
 	}
 	return (dest);
 }
