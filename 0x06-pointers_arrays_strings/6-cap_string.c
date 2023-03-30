8e45a00ffd38ee4968f844d05483f92243ad536e
tibebu <tibebu.solomonhi@gmail.com>
2023-03-30 07:21:37 -0400

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,14 +13,21 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-		if (str[i - 1] == ' ' || str[i - 1 == '\t' ||
-				str[i - 1] == '\n' || str[i - 1 == ',' ||
-				str[i - 1] == ';' || str[i - 1 == '.' ||
-				str[i - 1] == '!' || str[i - 1 == '?' ||
-				str[i - 1] == '-' || str[i - 1 == '(' ||
-				str[i - 1] == ')' || str[i - 1 == '{' ||
-				str[i - 1] == '}' || str[i - 1 == 0)
-			str[i] -= 32;
+		if (str[i - 1] == ' ' |
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '-' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		str[i - 1] == 0)
+		str[i] -= 32;
 		i++;
 	}
 	return (str);
