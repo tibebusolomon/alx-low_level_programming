756eaabf8bcf1c8ea7cc6355eb0540198f160ed0
tibebu <tibebu.solomonhi@gmail.com>
2023-04-08 18:19:23 -0400

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,45 +2,6 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the converted int from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-	while (s[len] != '\0')
-		len++;
-	while (i < len &&  f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-		return (0);
-	return (0);
-}
-
-/**
  * main - multiplies two numbers.
  * @argc: This is the number of arguments
  * @argv: This is the array of arguments
@@ -57,8 +18,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	product = num1 * num2;
 	printf("%d\n", product);
 	return (0);
